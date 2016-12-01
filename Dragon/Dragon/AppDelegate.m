5de8c09d5552ca6e84bd41412812ec7a0eaa5841
nvdabuzhongliu <nvbuzhongliu@gmail.com>
2016-12-01 12:37:03 +0800

    nslog

--- Dragon/Dragon/AppDelegate.m
+++ Dragon/Dragon/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"刘江伟-1232323");
     return YES;
 }
 
